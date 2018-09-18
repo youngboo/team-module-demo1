67c3210d65fcd83634a9b7253297df6a64855297
youngboo <504142201@qq.com>
2018-09-18 15:46:24 +0800

    编译tsx

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,34 +1,31 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-              from: './src/components',
-              to: './lib'
-            }
-      
-          ])
-    ],
+    // module: {
+    //     rules: [
+    //         // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+    //         { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
+    //         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    //         { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    //     ]
+    // }
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+          {
+            test: /\.tsx?$/,
+            use: [
+              {
+                loader: 'ts-loader',
+                options: {
+                  transpileOnly: true
+                }
+              }
+            ]
+          }
         ]
-    }
+      }
 };
